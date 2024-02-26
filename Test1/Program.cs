7d1e7d00f49fdb0bde52dcbda2c366dde53ab24b
Alyona_Koniukhova <122790344+Nolove0905@users.noreply.github.com>
2024-02-26 18:25:35 +0300

    Добавьте файлы проекта.

---
+++ Test1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение x: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        int y = 5 * x + 20;
+
+        Console.WriteLine($"Значение выражения y = 5 * x + 20 равно: {y}");
+    }
+}
